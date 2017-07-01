be8726c7411ff1d9fdf70de113f8987a5823210f
Thomas Klaner <thomas@iwa-tech.com>
2017-07-02 01:22:47 +0200

    added method as label

--- src/tracker.ts
+++ src/tracker.ts
@@ -8,13 +8,13 @@ export class Tracker {
   private responseTime = new client.Histogram({
     name: 'response_time_histogram',
     help: 'Response time histogram',
-    labelNames: ['name', 'uri', 'status'],
+    labelNames: ['name', 'uri', 'method', 'status'],
   });
 
   private requestCounter = new client.Counter({
     name: 'request_counter',
     help: 'Counter of all requests',
-    labelNames: ['name', 'uri', 'status'],
+    labelNames: ['name', 'uri', 'method', 'status'],
   });
 
   public static toFloat(timeTuple: [number, number]) {
@@ -36,10 +36,10 @@ export class Tracker {
   }
 
   public static getInstance(name: string, port: number = 9090, collectDefaultMetrics: boolean = true) {
-    return new Tracker(name, port);
+    return new Tracker(name, port, collectDefaultMetrics);
   }
 
-  private constructor(private name: string, port: number, collectDefaultMetrics: boolean = true) {
+  private constructor(private name: string, port: number, collectDefaultMetrics: boolean) {
     if (collectDefaultMetrics) {
       // Probe every 5th second.
       client.collectDefaultMetrics(5000);
@@ -52,9 +52,9 @@ export class Tracker {
     }).listen(port);
   }
 
-  public trackResponseTime(uri: string, status: number, seconds: number) {
-    this.responseTime.labels(this.name, uri, status.toString()).observe(seconds);
-    this.requestCounter.labels(this.name, uri, status.toString()).inc();
+  public trackResponseTime(uri: string, method: string, status: number, seconds: number) {
+    this.responseTime.labels(this.name, uri, method, status.toString()).observe(seconds);
+    this.requestCounter.labels(this.name, uri, method, status.toString()).inc();
   }
 
   public getMetrics(): any {
